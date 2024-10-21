345ff4149211ac777367c0e3c20f0d8f26873954
Falghannami <falghannami24@twq.idserve.net>
2024-10-21 10:27:34 +0300

    Initial Commit

---
+++ LearningjourneyTests/LearningjourneyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningjourneyTests.swift
+//  LearningjourneyTests
+//
+//  Created by فالحه الغنامي on 18/04/1446 AH.
+//
+
+import Testing
+@testable import Learningjourney
+
+struct LearningjourneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
